c8b0ffda38c8c7b39633db3a5a1d160d206cff70
Mou <madalmou.2004@gmail.com>
2024-03-02 02:40:09 +0530

    Qrcode folder is added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+const text = ref('');
+const qr = ref(
+    {
+        src: '',
+    }
+);
+const imgbox = ref(null);
+const qrtext = ref(null);
+const data = () => {
+    if (text.value.length > 0) {
+        qr.value.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text.value}`;
+        imgbox.value.classList.add("show-img");
+    } else {
+        qrtext.value.classList.add("show-img");
+        setTimeout(() => {
+            qrtext.value.classList.remove("show-img");
+        }, 1000);
+    }
+};
+</script>
+<template>
+    <div class="container">
+        <div class="wrap">
+            <p>Enter your text or URL</p>
+            <input type="text" placeholder="Enter your text or URL" v-model="text">
+        </div>
+        <div :class="imgbox">
+            <img :src="qr.src" ref="imgbox">
+        </div>
+        <button @click="data">Generate Qr Code</button>
+    </div>
+</template> 
